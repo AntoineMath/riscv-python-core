da5d1e88971377154128684f781998a21d3af7d7
AntoineMath <antoine.mathu@gmail.com>
2023-02-14 13:11:01 +0100

    milestone: successful first test

--- regfile.py
+++ regfile.py
@@ -6,7 +6,7 @@ from elftools.elf.elffile import ELFFile
 # TODO: understand why instruction at 80002a7c is 0b0 for riscv-tests/isa/rv32ui-v-add
 
 # 8192 bytes at 0x80000000
-memory = b'\x00'*0x10000
+memory = b'\x00'*0x4000
 
 regfile = [0]*33
 PC = 32
@@ -36,6 +36,10 @@ class Funct3(Enum):
   XORI = 0b100
   ORI = 0b110
   ANDI = 0b011
+  BEQ = 0b000
+  BNE = 0b001
+  BLT = 0b100
+  BGE = 0b101
 
 class Funct7(Enum):
   ADD = 0b0000000 
@@ -66,6 +70,11 @@ def dump():
   pp += "\n"
   print("".join(pp))
 
+def sign_extend(x, l):
+  if x >> (l-1) == 1:
+    return -((1 << l) - x)
+  else:
+    return x
 
 def step():
   # Instruction fetch
@@ -76,90 +85,113 @@ def step():
     # little endian (so start is at the right of the word)
     return (instruction >> s) & ((1 << e-s+1)-1)
 
-  print(bin(gib(0,6)))
   opcode = Ops(gib(0, 6))
+  print("%r  %r" % (hex(regfile[PC]), opcode))
+  imm_u = gib(12, 31)
+  rd = gib(7, 11)
 
   if opcode == Ops.JAL:
-    print("JAL")
+    # J-TYPE
     rd = gib(7, 11)
-    imm20 = gib(30, 31) << 20
+    imm20 = gib(31, 32) << 20
+    # Note : we shift << 1 and not << 0 because we want to be sure instruction is located at an even address.
     imm1 = gib(21, 30) << 1
     imm11 = gib(20, 21) << 11
     imm12 = gib(12, 19) << 12
     offset = imm20 | imm1 | imm11 | imm12
-    regfile[PC] += offset
     regfile[rd] = regfile[PC] + 4
+    regfile[PC] += offset
+    print("PC at: ", hex(regfile[PC]))
+    return True
+
+  elif opcode == Ops.JALR:
+    # J type
+    rs1 = gib(15, 19)
+    imm = gib(20, 31)
+    regfile[rd] = regfile[PC] + 4
+    regfile[PC] += offset
+    return True
 
   elif opcode == Ops.IMM:
-    rd = gib(7, 11) # used for pseudo MV instuction
     rs1 = gib(15, 19)
     imm = gib(20, 31)
     funct3 = Funct3(gib(12, 14))
 
     if funct3 == Funct3.ADDI:
+      #print("ADDI")
       regfile[rd] = regfile[rs1] + imm
-      print(f"ADDI {hex(rd)}, zero, {rs1}") #TODO: should be -128, check that it working
+      #print(f"ADDI {rd}, {hex(regfile[rs1])}, {imm}") #TODO: should be -128, check that it working
 
     elif funct3 == Funct3.SLLI:
       shamt = gib(20, 24)
       regfile[rd] = regfile[rs1] << shamt & 0xFFFFFFFF
       print(f"SLLI {rd}, {rs1}, {shamt}")
-
-    regfile[PC] += 4
+    else: raise Exception("%r funct3: %r" % (opcode, funct3))
 
   elif opcode == Ops.OP:
-    rd = gib(7, 11) # used for pseudo MV instuction
     rs1 = gib(15, 19)
     imm = gib(20, 31)
     funct3 = Funct3(gib(12, 14))
     funct7 = Funct7(gib(25, 31))
 
-    if funct7 == Funct7.ADD:
-      print(f"ADD {hex(rd)}, {hex(imm)}, {hex(rs1)}") #TODO: should be -128, check that it working
-    if funct7 == Funct7.SUB:
-      print(f"SUB {hex(rd)}, hex{imm}, {rs1}") #TODO: should be -128, check that it working
-    regfile[rd] = regfile[rs1] + regfile[imm] 
-
-    regfile[PC] += 4
+    if funct7 == Funct7.ADD | funct7 == Funct7.SUB:
+      regfile[rd] = regfile[rs1] + regfile[imm] 
+    else: raise Exception("%r funct7: %r" % (opcode, funct7))
 
   elif opcode == Ops.AUIPC:
-    ins = Uinstruction(instruction)
-    offset = ins.imm << 12 
-    regfile[ins.rd] = regfile[PC] + offset
-    regfile[PC] += 4
+    # U Type
+    regfile[rd] = regfile[PC] + imm_u
+
+  elif opcode == Ops.LUI:
+    # U Type
+    regfile[rd] = imm_u << 12
+    
   
+
+  elif opcode == Ops.BRANCH:
+    # B TYPE 
+    # Note : we shift << 1 and not << 0 because we want to be sure instruction is located at an even address.
+    offset = gib(31, 32) << 12 | gib(25, 30) << 5 | gib(8, 11 << 1) | (gib(7,8)) << 11
+    funct3 = Funct3(gib(12, 14))
+    rs1 = gib(15, 19)
+    rs2 = gib(20, 24)
+    condition = False
+
+    if funct3 == Funct3.BEQ:
+      condition  = regfile[rs1] == regfile[rs2]
+    elif funct3 == Funct3.BNE:
+      condition = regfile[rs1] != regfile[rs2]
+    elif funct3 == Funct3.BLT:
+      condition = sign_extend(regfile[rs1], 32) < sign_extend(regfile[rs2], 32)
+    elif funct3 == Funct3.BLTU:
+      condition = regfile[rs1] < regfile[rs2]
+    elif funct3 == Funct3.BGE:
+      condition  = sign_extend(regfile[rs1], 32) >= sign_extend(regfile[rs2], 32)
+    elif funct3 == Funct3.BGEU:
+      condition  = regfile[rs1] >= regfile[rs2]
+    else:
+      raise Exception("instruction: %r funct3 %r" % (opcode, funct3))
+
+    if condition:
+      regfile[PC] += offset
+      return True
+
   elif opcode == Ops.SYSTEM: # TODO: understand that
-    print("SYSTEM (don't know what it is)")
-    regfile[PC] += 4
-  
-  #elif opcode == Ops.Op:
+    # Type I
+    pass
+  elif opcode == Ops.MISC: # sytem call ? 
+    pass
 
   else:
-    print(hex(instruction), bin(instruction))
-    dump()
-    return False
+    raise Exception("Opcode %r not known" % (opcode) )
 
-  dump()
+  regfile[PC] += 4
+  #dump()
 
   return True 
-class Instruction:
-  def gib(self, s, e):
-    # little endian (so start is at the right of the word)
-    return (self.ins>> s) & ((1 << e-s+1)-1)
-
-class Uinstruction(Instruction):
-    # 31_______________12_11___7_6_________0
-    # |    imm[31:12]    |  rd  |  opcode  |
-    # |__________________|______|__________|
-  def __init__(self, ins):
-    self.type = "U" 
-    self.ins = ins
-    self.rd = self.gib(7, 11)
-    self.imm = self.gib(12, 31)
-
 
 if __name__ == "__main__":
-  for f in glob.glob("riscv-tests/isa/rv32ui-v*"):
+  for f in glob.glob("riscv-tests/isa/rv32ui-p-add*"):
     if f.endswith(".dump"): continue
     with open(f, 'rb') as f:
       print("test", f.name)
